a58c5882ac0ab1e16d56db9967515715cf2cbb6b
Igor Oliveira Valente da Silveira <igor.silveira@dtidigital.com.br>
2021-04-04 04:37:14 -0300

    Creating some entities

---
+++ Mnemopic.Borders/Entities/Image.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mnemopic.Borders.Entities
+{
+    public class Image
+    {
+        public Guid Id { get; set; }
+        public string Link { get; set; }
+        public int Number { get; set; }
+        public DateTime CreatedAd { get; set; }
+    }
+}
+
---
+++ Mnemopic.Borders/Entities/Word.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Mnemopic.Borders.Entities
+{
+    public class Word
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public Guid ImageId { get; set; }
+        public DateTime CreatedAd { get; set; }
+    }
+}
+
