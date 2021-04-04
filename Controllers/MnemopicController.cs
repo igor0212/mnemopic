c419a268d0a72eb355806ca7858fc1eb55e67a2f
Igor Oliveira Valente da Silveira <igor.silveira@dtidigital.com.br>
2021-04-04 03:54:14 -0300

    [#0001] Creating structure api

---
+++ Controllers/MnemopicController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+
+namespace Mnemopic.Controllers
+{
+    [Route("api/mnemopic")]
+    [ApiController]
+    public class MnemopicController : ControllerBase
+    {
+        private readonly ILogger<MnemopicController> _logger;
+
+        public MnemopicController(ILogger<MnemopicController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("{id}")]
+        public Borders.Entities.Mnemopic Get(Guid id)
+        {
+            var teste = new Borders.Entities.Mnemopic();
+            teste.Name = "ball";
+            teste.Link = "teste";
+
+            return teste;
+        }
+    }
+}
